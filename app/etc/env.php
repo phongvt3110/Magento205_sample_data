d8f7bea1a4da82bac58a04418e61e06b1a484455
phongvt3110 <vtphong651043@gmail.com>
2016-06-11 06:54:21 +0700

    upload source code to server

---
+++ app/etc/env.php
@@ -0,0 +1,58 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'admin_vtphong651043',
+  ),
+  'install' => 
+  array (
+    'date' => 'Mon, 16 May 2016 04:20:33 +0000',
+  ),
+  'crypt' => 
+  array (
+    'key' => '5c3e3d0947fcb0fb8a603aaa39f67fbb',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'magentoCE205',
+        'username' => 'root',
+        'password' => 'monkey3110',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+);
